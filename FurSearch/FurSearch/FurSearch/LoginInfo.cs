d937d18d563b813fd22129b1648fac5804765f11
Sylviatic <arca9ies@gmail.com>
2017-12-30 11:48:11 +0100

    For Monstah to fix - M.EDependencyInjection Failure

---
+++ FurSearch/FurSearch/FurSearch/LoginInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FurSearch
+{
+    public class LoginInfo
+    {
+        public string token = "MzUzMjY2OTI4NTk0MTkwMzM2.DSj3Mg.ogJ__LHhtV-17FMrTegA85DeAKc";
+    }
+}
